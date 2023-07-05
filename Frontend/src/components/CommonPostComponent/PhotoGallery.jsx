5ed3ede471fc7772d5cbd060a9c33386c7c9c4e9
Manish <mthefool218@gmail.com>
2023-07-06 01:41:27 +0530

    added the hash layer before image

--- Frontend/src/components/CommonPostComponent/PhotoGallery.jsx
+++ Frontend/src/components/CommonPostComponent/PhotoGallery.jsx
@@ -1,9 +1,20 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { Cross } from "../SVGs/SVGs";
+import { Blurhash } from "react-blurhash";
 
 const PhotoGallery = ({ photos, photo, index, deleteImages, mark }) => {
     const [imageHeight, setImageHeight] = useState(0);
+    const [imageLoaded, setImageLoaded] = useState(false);
+
+    useEffect(() => {
+        const img = new Image();
+        img.onload = () => {
+            setImageLoaded(true);
+        };
+        img.src = photo;
+    }, [photo]);
+
     const crossHandler = (e, photo) => {
         e.stopPropagation();
         deleteImages(photo);
@@ -38,12 +49,22 @@ const PhotoGallery = ({ photos, photo, index, deleteImages, mark }) => {
                             onClick={(e) => {
                                 e.stopPropagation();
                             }}>
-                            <img
-                                src={photo}
-                                alt="first image"
-                                loading="lazy"
-                                className={`${photos.length === 3 || photos.length === 2 ? `h-full w-full rounded-l-xl object-cover` : `${photos.length === 4 ? `h-full w-full rounded-tl-xl object-cover` : `h-full w-full object-cover`}`}`}
-                            />
+                            {imageLoaded ? (
+                                <img
+                                    src={photo}
+                                    alt="first image"
+                                    loading="lazy"
+                                    className={`${photos.length === 3 || photos.length === 2 ? `h-full w-full rounded-l-xl object-cover` : `${photos.length === 4 ? `h-full w-full rounded-tl-xl object-cover` : `h-full w-full object-cover`}`}`}
+                                />
+                            ) : (
+                                <div className="relative overflow-hidden">
+                                    <div
+                                        className={`h-[18rem] border-2 bg-[#f8f8f8] ${
+                                            photos.length === 3 || photos.length === 2 ? ` w-full rounded-l-xl object-cover ` : `${photos.length === 4 ? ` w-full rounded-tl-xl object-cover` : ` w-full object-cover`}`
+                                        }`}></div>
+                                    <div className="blurhash-shimmer"></div>
+                                </div>
+                            )}
                         </Link>
                     )}
                 </div>
@@ -71,12 +92,20 @@ const PhotoGallery = ({ photos, photo, index, deleteImages, mark }) => {
                             onClick={(e) => {
                                 e.stopPropagation();
                             }}>
-                            <img
-                                src={photo}
-                                alt="second image"
-                                loading="lazy"
-                                className={`${photos.length >= 3 ? `h-full w-full rounded-tr-xl object-cover` : `${photos.length === 2 ? `h-full w-full rounded-r-xl object-cover` : `h-full w-full object-cover`}`}`}
-                            />
+                            {imageLoaded ? (
+                                <img
+                                    src={photo}
+                                    alt="second image"
+                                    loading="lazy"
+                                    className={`${photos.length >= 3 ? `h-full w-full rounded-tr-xl object-cover` : `${photos.length === 2 ? `h-full w-full rounded-r-xl object-cover` : `h-full w-full object-cover`}`}`}
+                                />
+                            ) : (
+                                <div className="relative overflow-hidden">
+                                    <div
+                                        className={`h-[18rem] w-full border-2 bg-[#f8f8f8] ${photos.length >= 3 ? ` w-full rounded-tr-xl object-cover` : `${photos.length === 2 ? ` w-full rounded-r-xl object-cover` : ` w-full object-cover`}`}`}></div>
+                                    <div className="blurhash-shimmer"></div>
+                                </div>
+                            )}
                         </Link>
                     )}
                 </div>
@@ -104,12 +133,22 @@ const PhotoGallery = ({ photos, photo, index, deleteImages, mark }) => {
                             onClick={(e) => {
                                 e.stopPropagation();
                             }}>
-                            <img
-                                src={photo}
-                                alt="third image"
-                                loading="lazy"
-                                className={`${photos.length === 3 ? `h-full w-full rounded-br-xl object-cover` : `${photos.length === 4 ? `h-full w-full rounded-bl-xl object-cover` : `h-full w-full object-cover`}`}`}
-                            />
+                            {imageLoaded ? (
+                                <img
+                                    src={photo}
+                                    alt="third image"
+                                    loading="lazy"
+                                    className={`${photos.length === 3 ? `h-full w-full rounded-br-xl object-cover` : `${photos.length === 4 ? `h-full w-full rounded-bl-xl object-cover` : `h-full w-full object-cover`}`}`}
+                                />
+                            ) : (
+                                <div className="relative overflow-hidden">
+                                    <div
+                                        className={` h-[9rem] w-full border-2 bg-[#f8f8f8] ${
+                                            photos.length === 3 ? ` w-full rounded-br-xl object-cover` : `${photos.length === 4 ? ` w-full rounded-bl-xl object-cover` : ` w-full object-cover`}`
+                                        }`}></div>
+                                    <div className="blurhash-shimmer"></div>
+                                </div>
+                            )}
                         </Link>
                     )}
                 </div>
@@ -133,7 +172,14 @@ const PhotoGallery = ({ photos, photo, index, deleteImages, mark }) => {
                             onClick={(e) => {
                                 e.stopPropagation();
                             }}>
-                            <img src={photo} loading="lazy" alt="fourth image" className={`${photos.length === 4 ? `h-full w-full rounded-br-xl object-cover` : `h-full w-full object-cover`}`} />
+                            {imageLoaded ? (
+                                <img src={photo} loading="lazy" alt="fourth image" className={`${photos.length === 4 ? `h-full w-full rounded-br-xl object-cover` : `h-full w-full object-cover`}`} />
+                            ) : (
+                                <div className="relative overflow-hidden">
+                                    <div className={`h-[9rem] w-full border-2 bg-[#f8f8f8] ${photos.length === 4 ? `h-full w-full rounded-br-xl object-cover` : `h-full w-full object-cover`}`}></div>
+                                    <div className="blurhash-shimmer"></div>
+                                </div>
+                            )}
                         </Link>
                     )}
                 </div>
@@ -157,7 +203,14 @@ const PhotoGallery = ({ photos, photo, index, deleteImages, mark }) => {
                         onClick={(e) => {
                             e.stopPropagation();
                         }}>
-                        <img src={photo} loading="lazy" alt="first image ,if only one photo" className={`h-full  w-full rounded-xl ${imageHeight > 30 * 16 ? "object-cover object-center" : "object-fill"}`} onLoad={(e) => imageHeightChecker(e)} />
+                        {imageLoaded ? (
+                            <img src={photo} loading="lazy" alt="first image ,if only one photo" className={`h-full  w-full rounded-xl  ${imageHeight > 30 * 16 ? "object-cover object-center" : "object-fill"}`} onLoad={(e) => imageHeightChecker(e)} />
+                        ) : (
+                            <div className="relative overflow-hidden">
+                                <div className={` h-[30rem] w-full bg-[#f8f8f8]`}></div>
+                                <div className="blurhash-shimmer"></div>
+                            </div>
+                        )}
                     </Link>
                 )}
             </div>
