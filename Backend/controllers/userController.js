09d8c5b1b46a5dc18018d00d3295f9a77a5800aa
Manish <mthefool218@gmail.com>
2023-10-04 00:40:42 +0530

    some changes to cookie

--- Backend/controllers/userController.js
+++ Backend/controllers/userController.js
@@ -105,7 +105,12 @@ exports.register = async (req, res, next) => {
 
             return res
                 .status(201)
-                .cookie("token", token, { expires: new Date(Date.now() + 30 * 24 * 60 * 60 * 1000), httpOnly: true })
+                .cookie("token", token, {
+                    expires: new Date(Date.now() + 30 * 24 * 60 * 60 * 1000),
+                    httpOnly: true,
+                    sameSite: "None",
+                    secure: true,
+                })
                 .json({
                     success: true,
                     user,
@@ -174,7 +179,7 @@ exports.login = async (req, res, next) => {
         const token = await user.generateToken();
 
         res.status(200)
-            .cookie("token", token, { expires: new Date(Date.now() + 30 * 24 * 60 * 60 * 1000), httpOnly: true })
+            .cookie("token", token, { expires: new Date(Date.now() + 30 * 24 * 60 * 60 * 1000), httpOnly: true, sameSite: "None", secure: true })
             .json({
                 success: true,
                 user,
